8acc4c2e070e687ba85d659bbea59a910a7a5540
Ziyue <zyang43@ur.rochester.edu>
2021-11-24 15:34:03 -0500

    test mlp

--- alpdesign/mlp.py
+++ alpdesign/mlp.py
@@ -17,18 +17,18 @@ class EnsembleBlockConfig:
         64,
         2,
     )
-    model_number: int = 5
+    model_number: int = 1
 
 
 @dataclass
 class AlgConfig:
-    train_epochs: int = 200
+    train_epochs: int = 6
     train_batch_size: int = 8
     train_lr: float = 1e-2
     train_adam_b1: float = 0.8
     train_adam_b2: float = 0.9
     train_adam_eps: float = 1e-4
-    weight_decay: float = 1e-3
+    weight_decay: float = 1e-2
     bo_epochs: int = 500
     bo_lr: float = 1e-2
     bo_xi: float = 1e-1
@@ -42,8 +42,11 @@ class SingleBlock(hk.Module):
         self.config = config
 
     def __call__(self, x):
+        key = hk.next_rng_key()
         for idx, dim in enumerate(self.config.shape):
             x = hk.Linear(dim)(x)
+            if idx == 0:
+                x = hk.dropout(key, 0.5, x)
             if idx < len(self.config.shape) - 1:
                 x = jax.nn.relu(x)
                 #if idx > 0:
@@ -69,6 +72,25 @@ class EnsembleBlock(hk.Module):
         return out
 
 
+class NaiveBlock(hk.Module):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+
+    def __call__(self, x):
+        out = hk.nets.MLP((256, 128, 64, 1,))(x)
+        return out
+
+def naive_forward(x):
+    f = NaiveBlock()
+    return f(x)
+naive_forward_t = hk.transform(naive_forward)
+
+
+def _naive_loss(forward, key, params, seq, label):
+    yhat = forward(params, key, seq) #scalar
+    return (label-yhat)**2
+
+
 def _transform_var(s):
     # heuristic to make MLP output better behaved.
     return jax.nn.softplus(s) + 1e-6
@@ -78,7 +100,8 @@ def model_reduce(out):
     mu = jnp.mean(out[..., 0], axis=0)
     var = jnp.mean(_transform_var(
         out[..., 1]) + out[..., 0] ** 2, axis=0) - mu ** 2
-
+    #var = jnp.mean(_transform_var(
+        #out[..., 1]), axis=0) + jnp.std(out[..., 0], axis=0)
     return mu, var
 
 
@@ -114,26 +137,37 @@ def _deep_ensemble_loss(params, key, forward, seqs, labels):
     return jnp.sum(n_log_likelihoods, axis=0)
 
 
+def _single_loss(forward, key, params, seqs, labels):
+    out = forward(params, key, seqs)
+    yhats = out[..., 0]
+    mse = jnp.mean((yhats-labels)**2, axis=0)
+    return mse
+
+
 def _adv_loss_func(forward, M, params, key, seq, label):
     # first tile sequence/labels for each model
     seq_tile = jnp.tile(seq, (M, 1))
     label_tile = jnp.tile(label, M)
-    epsilon = 1e-2
+    epsilon = 1e-3
     key1, key2 = jax.random.split(key)
-    #grad_inputs = jax.grad(_deep_ensemble_loss, 3)(
-    #    params, key, forward, seq_tile, label_tile
-    #)
+    grad_inputs = jax.grad(_deep_ensemble_loss, 3)(
+        params, key, forward, seq_tile, label_tile
+    )
 
     def _var(params, forward, seqs):
         out = forward(params, key, seqs)
         return jnp.sum(_transform_var(out[..., 1]))
 
-    grad_inputs = jax.grad(_var, 2)(params, forward, seq_tile)
+    #grad_inputs = jax.grad(_var, 2)(params, forward, seq_tile)
+    #single_grad_inputs = jax.grad(_single_loss, 3)(params, key, forward, seq_tile, label_tile)
     seqs_ = seq_tile + epsilon * jnp.sign(grad_inputs)
-
+    #seqs_ = seq_tile + epsilon * jnp.sign(single_grad_inputs)
     return _deep_ensemble_loss(
         params, key1, forward, seq_tile, label_tile
     ) + _deep_ensemble_loss(params, key2, forward, seqs_, label_tile)
+    #return _single_loss(params, key1, forward, seq_tile, label_tile
+    #    ) + _single_loss(params, key2, forward, seqs_, label_tile)
+
 
 
 def _hard_sampling(a, b, head_rate=0.3, tail_rate=0.2):
@@ -184,14 +218,15 @@ def ensemble_train(
         aconfig = AlgConfig()
     opt_init, opt_update = optax.chain(
         optax.clip_by_global_norm(aconfig.global_norm),
-        optax.scale_by_adam(
-            b1=aconfig.train_adam_b1,
-            b2=aconfig.train_adam_b2,
-            eps=aconfig.train_adam_eps,
-        ),
+        #optax.scale_by_adam(
+        #    b1=aconfig.train_adam_b1,
+        #    b2=aconfig.train_adam_b2,
+        #    eps=aconfig.train_adam_eps,
+        #),
         optax.add_decayed_weights(aconfig.weight_decay),
-        optax.scale(-aconfig.train_lr),  # minus sign -- minimizing the loss
-        #optax.scale_by_schedule(optax.cosine_decay_schedule(-1e-1, 50)),
+        #optax.scale(-aconfig.train_lr),  # minus sign -- minimizing the loss
+        #optax.scale_by_schedule(optax.cosine_decay_schedule(-1e-2., 50)),
+        optax.adam(optax.cosine_onecycle_schedule(500, 5e-2))
      )
 
     key, bkey = jax.random.split(key)
@@ -212,6 +247,7 @@ def ensemble_train(
     loss_fxn = lambda *args: jnp.mean(
         jax.vmap(adv_loss, in_axes=(None, None, 0, 0))(*args)
     )
+    print('training in progress')
 
     @jax.jit
     def train_step(opt_state, params, key, seqs, labels):
@@ -222,17 +258,23 @@ def ensemble_train(
 
     losses = []
     val_losses = []
-    for e in range(aconfig.train_epochs // (len(labels) // aconfig.train_batch_size)):
+    #for e in range(aconfig.train_epochs // (len(labels) // aconfig.train_batch_size)):
+    for e in range(aconfig.train_epochs): 
         # shuffle seqs and labels
         key, tkey = jax.random.split(key, num=2)
         #hard_seqs, hard_labels = _hard_sampling(seqs, labels)
-        shuffle_seqs, shuffle_labels = _shuffle_in_unison(key, seqs, labels)
+        #shuffle_seqs, shuffle_labels = _shuffle_in_unison(key, seqs, labels)
+        train_loss = 0.
+        shuffle_seqs, shuffle_labels = seqs, labels
         for i in range(0, len(shuffle_labels) // aconfig.train_batch_size):
             seq = shuffle_seqs[i: (i + 1) * aconfig.train_batch_size]
             label = shuffle_labels[i: (i + 1) * aconfig.train_batch_size]
             opt_state, params, loss = train_step(
                 opt_state, params, tkey, seq, label)
-            losses.append(loss)
+            train_loss += loss
+        train_loss /= len(shuffle_labels) // aconfig.train_batch_size
+        losses.append(train_loss)
+            #losses.append(loss)
         # compute validation loss
         if val_seqs is not None:
             val_loss = 0.0
@@ -250,6 +292,66 @@ def ensemble_train(
     return (params, losses) if val_seqs is None else (params, losses, val_losses)
 
 
+def naive_train(key, forward_t, seqs, labels, val_seqs=None, val_labels=None, params=None, aconfig: AlgConfig = None):
+    if aconfig is None:
+        aconfig = AlgConfig()
+    opt_init, opt_update = optax.chain(
+        optax.scale_by_adam(b1=0.8, b2=0.9, eps=1e-4),
+        optax.scale(-aconfig.train_lr)  # minus sign -- minimizing the loss
+    )
+
+    key, bkey = jax.random.split(key)
+    # fill in seqs/labels if too small
+    seqs, labels = _fill_to_batch(seqs, labels, bkey, aconfig.train_batch_size)
+
+    if params == None:
+        params = forward_t.init(key, seqs[0])
+
+    opt_state = opt_init(params)
+    # wrap loss in batch/sum
+    loss_ = partial(_naive_loss, forward_t.apply)
+    loss_fxn = lambda *args: jnp.mean(jax.vmap(loss_, in_axes=(None, None, 0, 0))(*args))
+    #loss_fxn = jnp.mean(jax.vmap(loss_, in_axes=(None, 0, 0)))
+
+    @jax.jit
+    def train_step(opt_state, params, key, seqs, labels):
+        loss, grad = jax.value_and_grad(loss_fxn, 1)(
+            key, params, seqs, labels)
+        updates, opt_state = opt_update(grad, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return opt_state, params, loss
+    losses = []
+    val_losses = []
+    
+    #for e in range(aconfig.train_epochs // (len(labels) // aconfig.train_batch_size)):
+    for e in range(aconfig.train_epochs):
+        # shuffle seqs and labels
+        key, key_ = jax.random.split(key, num=2)
+        shuffle_seqs, shuffle_labels = _shuffle_in_unison(key, seqs, labels)
+        train_loss = 0.
+        for i in range(0, len(shuffle_labels) // aconfig.train_batch_size):
+            seq = shuffle_seqs[i:(i+1) * aconfig.train_batch_size]
+            label = shuffle_labels[i:(i+1) * aconfig.train_batch_size]
+            opt_state, params, loss = train_step(opt_state, params, key, seq, label)
+            train_loss += loss
+        train_loss /= len(shuffle_labels) // aconfig.train_batch_size
+        losses.append(train_loss)
+        # compute validation loss
+        if val_seqs is not None:
+            val_loss = 0.
+            for i in range(0, len(val_labels) // aconfig.train_batch_size):
+                seq = shuffle_seqs[i:(i+1) * aconfig.train_batch_size]
+                label = shuffle_seqs[i:(i+1) * aconfig.train_batch_size]
+                val_loss += loss_fxn(
+                    key,
+                    params,
+                    val_seqs[i:(i+1) * aconfig.train_batch_size],
+                    val_labels[i:(i+1) * aconfig.train_batch_size])
+            val_loss = val_loss/ (len(val_labels) //  aconfig.train_batch_size)
+            val_losses.append(val_loss)
+    return (params, losses) if val_seqs is None else (params, losses, val_losses)
+
+
 def neg_bayesian_ei(key, f, x, Y, xi):
     joint_out = f(key, x)
     mu = joint_out[0]
@@ -260,22 +362,34 @@ def neg_bayesian_ei(key, f, x, Y, xi):
     return -((mu - best - xi) * norm.cdf(z) + std * norm.pdf(z))
 
 
+def neg_bayesian_ucb(key, f, x, beta=2.):
+    joint_out = f(key, x)
+    mu = joint_out[0]
+    std = jnp.sqrt(joint_out[1])
+    ucb = mu + beta * std
+    return ucb
+
+
 def bayes_opt(key, f, labels, init_x, aconfig: AlgConfig = None, bo_xi=1e-1):
     if aconfig is None:
         aconfig = AlgConfig()
-    #optimizer = optax.adam(aconfig.bo_lr)
+    optimizer = optax.adam(-aconfig.bo_lr)
     #optimizer = optax.adam(optax.cosine_decay_schedule(1e-1, 50))
-    optimizer = optax.adam(optax.cosine_onecycle_schedule(500, 5e-2))
+    #optimizer = optax.adam(optax.cosine_onecycle_schedule(500, 5e-2))
     opt_state = optimizer.init(init_x)
     x = init_x
 
     # reduce it so we can take grad
-    reduced_neg_bayesian_ei = lambda *args: jnp.mean(neg_bayesian_ei(*args))
+    #reduced_neg_bayesian_ei = lambda *args: jnp.mean(neg_bayesian_ei(*args))
+    reduced_neg_bayesian_ucb = lambda *args: jnp.mean(neg_bayesian_ucb(*args))
 
     @jax.jit
     def step(x, opt_state, key):
-        loss = neg_bayesian_ei(key, f, x, labels, bo_xi)
-        g = jax.grad(reduced_neg_bayesian_ei, 2)(key, f, x, labels, bo_xi)
+        #loss = neg_bayesian_ei(key, f, x, labels, bo_xi)
+        #g = jax.grad(reduced_neg_bayesian_ei, 2)(key, f, x, labels, bo_xi)
+        loss = neg_bayesian_ucb(key, f, x)
+        g = jax.grad(reduced_neg_bayesian_ucb, 2)(key, f, x)
+
         updates, opt_state = optimizer.update(g, opt_state)
         x = optax.apply_updates(x, updates)
         return x, opt_state, loss
